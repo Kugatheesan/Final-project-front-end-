2f0a98f205d3c787b6e73cf366cfc5a7e6985e11
kugatheesan <vkugatheesan@gmail.com>
2025-01-23 16:08:15 +0530

    first change

---
+++ src/NavBar.tsx
@@ -0,0 +1,21 @@
+// import banner from ''
+
+function NavBar(){
+    return(
+        <>
+        <div className="first Page">
+            <div className="logoimg">
+                <img alt="" />
+            </div>
+            <div>
+                <h3>Home</h3>
+                <h3>About</h3>
+                <h3>Services</h3>
+                <h3>Booking</h3>
+                <h3>Login</h3>
+
+            </div>
+        </div>
+        </>
+    )
+}
